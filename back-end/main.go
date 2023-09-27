b8ce20ebc7db28744a849ab7e4e5ac2a8c8452c3
LuizFJP <luizfjportela@gmail.com>
2023-09-27 17:40:15 -0300

    starting backend

---
+++ back-end/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKey, err := crypto.HexToECDSA("5650e13409842ced70d7c30268dde9b38246d0450ef8dca4af89b9fcac6e01bf") // This is a private key, paste without 0x
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(1000000000000000000) // in wei (1 eth)
+	gasLimit := uint64(21000)                // in units
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toAddress := common.HexToAddress("0xce8664f4326ea1de2A9B94d2eb5f481a6827A5AE") // This is a wallet address
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID := big.NewInt(1337)
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s\n", signedTx.Hash().Hex()) // transaction sent
+
+	receipt, err := client.TransactionReceipt(context.Background(), signedTx.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+	if receipt == nil {
+		log.Fatal("receipt is nil. Forgot to commit?")
+	}
+
+	fmt.Printf("status: %v\n", receipt.Status) // status: 1
+
+	bl, err := client.BalanceAt(context.Background(), fromAddress, big.NewInt(5))
+	if err != nil {
+		log.Fatal("couldn't get balance", err)
+	}
+
+	fmt.Printf("balance: %v", bl)
+}
